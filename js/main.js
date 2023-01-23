725d76b8b0b9a5b4b01fe71f57f697b11a211702
Vini <barbosavinicius410@gmail.com>
2023-01-23 00:52:40 -0300

    teste

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const boraTomaUma = document.querySelector('.boratoma');
+    if(boraTomaUma){
+        boraTomaUma.muted = false;
+    }
+});
