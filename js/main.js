cf4997df50c835860322b60880ee485050f4a47c
Vini <barbosavinicius410@gmail.com>
2023-01-23 02:51:47 -0300

    teste de botão

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const boraTomaUma = document.querySelector('.boratoma');
-    if(boraTomaUma){
-        boraTomaUma.muted = false;
-    }
+const playButton = document.getElementById("play-button");
+const video = document.querySelector('.boratoma');
+
+playButton.addEventListener("click", function(){
+    video.muted = false;
+    video.play();
+    playButton.remove();
 });
